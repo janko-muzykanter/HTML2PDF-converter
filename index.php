1549cf7e7839aa4147d886e8f5073f1a5ba4671f
tomek <tomasz.kozielski@mksiegowa.pl>
2019-11-27 13:14:12 +0100

    Added index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+//ini_set('display_errors', 1);
+$root = __DIR__;
+//
+session_start();
+
+echo "<html><head>
+  </head>
+  <body><h1>Welcome to the Simple HTML->PDF generator<h1></body></html>";
+
+echo "<script src=js/bundle.js></script>";
+
+?>
