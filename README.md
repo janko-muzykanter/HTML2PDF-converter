c38778a943eee3424fd03eea22c712d7c0b999a4
tomek <tomasz.kozielski@mksiegowa.pl>
2019-11-27 13:34:15 +0100

    chmod warning

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # HTML2PDF-converter
+
+HTML2PDF-converter allows to save PDF files on the server based on submitted HTML code.
+
+STEPS to start:
+  1. composer install
+  2. Make sure server has access to __DIR__/files/ directory
+  3. Have fun!
--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <?php
 
-//ini_set('display_errors', 1);
+ini_set('display_errors', 1);
 $root = __DIR__;
 //
 session_start();
 
-echo "<html><head>
-  </head>
-  <body><h1>Welcome to the Simple HTML->PDF generator<h1></body></html>";
+$warning = substr(sprintf('%o', fileperms('files/')), -4) != '777' ? '(Warning: make sure __DIR__/files/ directory is writable for Apache)' : '';
+
+echo sprintf("<html><head>
+  </head><body>
+  <h1 style='margin-top: 60px'>Welcome to the Simple HTML->PDF generator<h1>
+  <h5 style='color: red;'>%s</h5>
+  </body></html>", $warning);
 
 echo "<script src=js/bundle.js></script>";
 
